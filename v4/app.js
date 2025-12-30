f0f1921bdfc595e0ed79d82a2c187948bdb4c818
AliAffan88 <122679452+AliAffan88@users.noreply.github.com>
2025-12-30 17:02:48 +0500

    Update app.js

--- v4/app.js
+++ v4/app.js
@@ -126,8 +126,6 @@ UndoRedo.onChangeSet(()=>{});
 function capture(){ UndoRedo.capture(snapshotState()); }
 
 //Lock Background Check
-const lockBgChk = document.getElementById('lockBgChk');
-
 lockBgChk.addEventListener('change', () => {
   const bg = canvas.querySelector('#bgImage');
   if (!bg) return;
